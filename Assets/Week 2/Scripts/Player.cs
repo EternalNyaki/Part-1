5099cdee3168bfcdc8adc01f22db00072728648c
MSI\etern <reyjoshu@sheridancollege.ca>
2024-01-18 09:50:30 -0500

    Created player and wall and made basic physics (also an extra special thing with the camera)

---
+++ Assets/Week 2/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float forceMultiplier = 100;
+
+    private Vector2 direction;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.AddForce(direction * forceMultiplier * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+}
